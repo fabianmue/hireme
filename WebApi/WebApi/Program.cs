009f103c0d48c2b6e578333dea6e688835c60b59
Fabian Mueller <fabian.mueller@gvz.ch>
2024-01-03 23:07:13 +0100

    feature: add migrations, add launch settings document dotnet ef tasks

--- WebApi/WebApi/Program.cs
+++ WebApi/WebApi/Program.cs
@@ -1,8 +1,22 @@
+using HireMe.WebApi.Shared;
 using HireMe.WebApi.Skills;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAutoMapper(typeof(SkillProfile));
 builder.Services.AddControllers();
+builder.Services.AddDbContext<WebApiContext>(options =>
+{
+    var connectionStringBuilder = new NpgsqlConnectionStringBuilder
+    {
+        Host = builder.Configuration.GetValue<string>("HIREME_WEBAPI_DB_HOST")!,
+        Database = builder.Configuration.GetValue<string>("HIREME_WEBAPI_DB_DATABASE")!,
+        Username = builder.Configuration.GetValue<string>("HIREME_WEBAPI_DB_USERNAME")!,
+        Password = builder.Configuration.GetValue<string>("HIREME_WEBAPI_DB_PASSWORD")!,
+    };
+    options.UseNpgsql(connectionStringBuilder.ConnectionString);
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true);
@@ -10,7 +24,6 @@ builder.Services.Configure<RouteOptions>(options => options.LowercaseUrls = true
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
-app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
